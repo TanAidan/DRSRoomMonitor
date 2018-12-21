d582afbe6dbdff2432639edda2db7f2adb71fb8c
PillowLord <33503144+PillowLord@users.noreply.github.com>
2018-12-21 13:02:24 -0800

    Added current date

--- app/src/main/java/com/example/tana5915/drsroommonitor/MainActivity.java
+++ app/src/main/java/com/example/tana5915/drsroommonitor/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.tana5915.drsroommonitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        TextView textViewDate = findViewById(R.id.text_view_date);
+        textViewDate.setText(currentDate);
+
+
     }
 }
---
+++ app/src/main/java/com/example/tana5915/drsroommonitor/Time.java
@@ -0,0 +1,10 @@
+package com.example.tana5915.drsroommonitor;
+
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class Time {
+
+}
