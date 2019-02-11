afcacb3141f60a992c1814a9808e8f3eff9c69d2
Aidan Tan <aidan.tan8@gmail.com>
2019-02-11 15:10:27 -0800

    done

--- app/src/main/java/com/example/tana5915/drsroommonitor/MainActivity.java
+++ app/src/main/java/com/example/tana5915/drsroommonitor/MainActivity.java
@@ -83,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         textViewDate.setText(currentDate);
         listView = (ListView) findViewById(R.id.list_view);
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        adapter.setCurrentMeetingPos(d.getMeetingIndex(currentMeeting, dayIndex));
         d.fill();
         for (int i = 0; i<d.getDayList().get(dayIndex).getMeetingList().size();i++) {
             list.add(d.getDayList().get(dayIndex).getMeetingList().get(i));
@@ -274,16 +275,17 @@ public class MainActivity extends AppCompatActivity {
             endTime.setText(eTime);
             startTime.setText(sTime);
         }
-        currentMeetingIndex=d.getMeetingIndex(currentMeeting);
+        currentMeetingIndex=d.getMeetingIndex(currentMeeting, dayIndex);
        // listView.setSelection(d.getMeetingIndex(currentMeeting));
-        Log.d("CurrentMeetingIndex", "index: "+ d.getMeetingIndex(currentMeeting));
-        adapter.setCurrentMeetingPos(d.getMeetingIndex(currentMeeting));
+        Log.d("CurrentMeetingIndex", "index: "+ d.getMeetingIndex(currentMeeting, dayIndex));
+        adapter.setCurrentMeetingPos(d.getMeetingIndex(currentMeeting, dayIndex));
        // listView.setItemChecked(d.getMeetingIndex(currentMeeting),true);
        // Log.d("CurrentMeeting", "CurrentMeeting Inndex"+ d.getMeetingIndex(currentMeeting));
     }
 
     public void nextDay(View view)
     {
+        adapter.setCurrentMeetingPos(d.getMeetingIndex(currentMeeting, dayIndex));
 
 
         if(dayIndex<14) {
@@ -297,6 +299,8 @@ public class MainActivity extends AppCompatActivity {
     }
     public void backDay(View view)
     {
+        adapter.setCurrentMeetingPos(d.getMeetingIndex(currentMeeting, dayIndex));
+
 
         if(dayIndex>0) {
             changeDate(false);
@@ -329,6 +333,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void changeDate(boolean direction)
     {
+
         if(direction)
         {
             calendar.add(Calendar.DATE,1);
--- app/src/main/java/com/example/tana5915/drsroommonitor/MeetingAdapter.java
+++ app/src/main/java/com/example/tana5915/drsroommonitor/MeetingAdapter.java
@@ -1,6 +1,7 @@
 package com.example.tana5915.drsroommonitor;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -23,8 +24,10 @@ public class MeetingAdapter extends ArrayAdapter<Meeting> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
+        Log.d("MeetingAdapter", "View Updated with index"+currentMeetingPos);
+
         final View renderer = super.getView(position, convertView, parent);
-        if (position == currentMeetingPos)
+        if (position == currentMeetingPos && currentMeetingPos!= -1)
         {
             //TODO: set the proper selection color here:
             renderer.setBackgroundResource(android.R.color.holo_orange_light);
